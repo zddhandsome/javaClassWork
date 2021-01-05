fe9eef11d044475a6481f6a72837fab5dd4ff5b4
zddhandsome <71329412+zddhandsome@users.noreply.github.com>
2021-01-05 14:28:19 +0800

    Add files via upload

---
+++ PolymorphismDemo.java
@@ -0,0 +1,15 @@
+package javaClassWork6;
+
+public class PolymorphismDemo {
+/**Main method*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+displayObject(new Circle(1,"red",false));
+displayObject(new Rectangle(1,1,"black",true));
+	}
+
+	/**Display geometric object properties*/
+	public static void displayObject(GeometricObject object) {
+	System.out.println("Created on "+object.getDateCreated()+" Color is "+ object.getColor());
+	}
+}
